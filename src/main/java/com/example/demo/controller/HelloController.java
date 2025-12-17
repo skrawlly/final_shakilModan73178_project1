c1908cf7f801bbce153ec360222fb181c788ea9c
shakilamirmodan <smodan1@stu.vistula.edu.pl>
2025-12-17 09:47:21 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,34 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+///import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HelloController {
+
+    @GetMapping(value="/")
+    @ResponseBody
+    public String hello() {
+        return "Hello Vistula, in my first Spring Controller!";
+    }
+
+    @GetMapping("/greeting")
+
+    public String greeting(
+            @RequestParam(name = "name", required = false, defaultValue = "World") String name,
+            Model model
+    ) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+
+
+
+}
+
+
